7f0a04cc3eb1184120a9aa212e29ca7af37eac16
bensongathee <benson7484@gmail.com>
2019-05-01 01:27:27 -0400

    trial1

---
+++ bmp.h
@@ -0,0 +1,159 @@
+// BMP-related data types based on Microsoft's own wingdi.h
+// Used from HarvardX CS50 with modifications by Varun Khatri
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <math.h>
+
+// aliases for C/C++ primitive data types
+// https://msdn.microsoft.com/en-us/library/cc230309.aspx
+typedef uint8_t  BYTE;
+typedef uint32_t DWORD;
+typedef int32_t  LONG;
+typedef uint16_t WORD;
+
+// information about the type, size, and layout of a file
+// https://msdn.microsoft.com/en-us/library/dd183374(v=vs.85).aspx
+typedef struct
+{
+    WORD bfType;
+    DWORD bfSize;
+    WORD bfReserved1;
+    WORD bfReserved2;
+    DWORD bfOffBits;
+}BITMAPFILEHEADER;
+
+// information about the dimensions and color format
+// https://msdn.microsoft.com/en-us/library/dd183376(v=vs.85).aspx
+typedef struct
+{
+    DWORD biSize;
+    LONG biWidth;
+    LONG biHeight;
+    WORD biPlanes;
+    WORD biBitCount;
+    DWORD biCompression;
+    DWORD biSizeImage;
+    LONG biXPelsPerMeter;
+    LONG biYPelsPerMeter;
+    DWORD biClrUsed;
+    DWORD biClrImportant;
+}BITMAPINFOHEADER;
+
+// relative intensities of red, green, and blue
+// https://msdn.microsoft.com/en-us/library/dd162939(v=vs.85).aspx
+typedef struct
+{
+    BYTE blue;
+    BYTE green;
+    BYTE red;
+}RGB;
+
+// Alternative representation of RGB model in terms of hue, saturation and value
+// http://learn.leighcotnoir.com/artspeak/elements-color/hue-value-saturation/
+typedef struct
+{
+    double hue;
+    double saturation;
+    double value;
+}HSV;
+
+// to covert one pixel's data from RGB to HSV domain
+// https://www.rapidtables.com/convert/color/rgb-to-hsv.html
+HSV rgb2hsv(RGB in)
+{   
+    HSV hsv;
+    // your code here
+    double Rprime = ((double) in.red / (double) 255);
+    double Gprime = ((double) in.green / (double) 255);
+    double Bprime = ((double) in.blue / (double) 255);
+    double rgbprimevals[] = {Bprime, Gprime, Rprime};
+    double Cmax = -1000;
+    double Cmin = 1000;
+    double delta = 1;
+    double hue;
+    double saturation;
+    double value;
+    int Cmaxrgbprimeval;
+    int i;
+
+    for(i = 0; i < 3; i++){
+        if(rgbprimevals[i] > Cmax){
+            Cmax = rgbprimevals[i];
+            Cmaxrgbprimeval = i;
+        }
+        if(rgbprimevals[i] < Cmin){
+            Cmin = rgbprimevals[i];
+        }
+    }
+
+    delta = Cmax - Cmin;
+
+    if(delta == 0){
+        hue = 0;
+    }else if(Cmaxrgbprimeval == 2){
+        hue = (60 * fmod((Gprime - Bprime)/delta, 6));
+    }else if(Cmaxrgbprimeval == 1){
+        hue = (60 * ((Bprime - Rprime)/delta+2));
+    }else if(Cmaxrgbprimeval == 0){
+        hue = (60 * ((Rprime - Gprime)/delta+4));
+    }
+
+    if(Cmax == 0){
+        saturation = 0;
+    }else if(Cmax != 0){
+        saturation = (delta/Cmax);
+    }
+
+    hsv.hue = hue;
+    hsv.saturation = saturation;
+    hsv.value = Cmax;
+
+    return hsv;
+}
+
+// to covert one pixel's data from HSV to RGB domain
+// https://www.rapidtables.com/convert/color/hsv-to-rgb.html
+RGB hsv2rgb(HSV in)
+{
+    RGB rgb;
+    // your code here
+    double C = in.value * in.saturation;
+    double X = C * (1- abs(fmod(in.hue/60,2)-1));
+    double m = in.value - C;
+    double Rprime;
+    double Gprime;
+    double Bprime;
+
+    if(in.hue>=0 && in.hue<60){
+        Rprime = C; 
+        Gprime = X;
+        Bprime = 0;
+    }else if(in.hue>=60 && in.hue<120){
+        Rprime = X; 
+        Gprime = C;
+        Bprime = 0;
+    }else if(in.hue>=120 && in.hue<180){
+        Rprime = 0; 
+        Gprime = C;
+        Bprime = X;
+    }else if(in.hue>=180 && in.hue<240){
+        Rprime = 0; 
+        Gprime = X;
+        Bprime = C;
+    }else if(in.hue>=240 && in.hue<300){
+        Rprime = X; 
+        Gprime = 0;
+        Bprime = C;
+    }else if(in.hue>=300 && in.hue<360){
+        Rprime = C; 
+        Gprime = 0;
+        Bprime = X;
+    }
+
+    rgb.red = (Rprime+m)*255;
+    rgb.blue = (Bprime+m)*255;
+    rgb.green = (Gprime+m)*255;
+
+    return rgb;
+}
---
+++ main.c
@@ -0,0 +1,213 @@
+#include <stdio.h>
+#include <string.h>
+#include "bmp.h"
+
+BITMAPFILEHEADER *ReadBMFileHeader(FILE *fp, FILE *fp2);
+BITMAPINFOHEADER *ReadBMInfoHeader(FILE *fp, FILE *fp2);
+RGB rgbtri(FILE *fp);
+DWORD ReadLittleEndian4(FILE *fp,FILE *fp2);
+WORD ReadLittleEndian2(FILE *fp,FILE *fp2);
+
+int main(int argc , char * argv[]){
+    FILE *fp;
+    FILE *fp2;
+    BITMAPFILEHEADER *bmpFileHeader = NULL;
+    BITMAPINFOHEADER *bmpInfoHeader = NULL;
+    RGB rgbtriple;
+    HSV hsvtriple;
+    unsigned char buffer; //delete
+
+    if(argc != 3){
+        puts("Usage: fileInputname fileOutputname");
+        exit(1);
+    }
+    if((fp = fopen(argv[1],"r")) == NULL){
+        printf("Cannot open file %s\n", argv[1]);
+        exit(1);
+    }
+    if((fp2 = fopen(argv[2], "r+")) == NULL){
+        printf("Cannot open file %s\n", argv[2]);
+        exit(1);
+    }
+
+    bmpFileHeader = ReadBMFileHeader(fp , fp2);
+    if(bmpFileHeader->bfType != 19778){
+        puts("File not a Bitmap");
+        exit(1);
+    }
+
+    bmpInfoHeader = ReadBMInfoHeader(fp, fp2);
+
+    printf("File type          = %i\n", bmpFileHeader->bfType);
+    printf("File size          = %d bytes\n", bmpFileHeader->bfSize);
+    printf("Data offset        = %ld bytes\n\n", bmpFileHeader->bfOffBits);
+    printf("Info header size   = %d bytes\n", bmpInfoHeader->biSize);
+    printf("Width              = %ld pixels\n", bmpInfoHeader->biWidth);
+    printf("Height             = %ld pixels\n", bmpInfoHeader->biHeight);
+    printf("Planes             = %d\n", bmpInfoHeader->biPlanes);
+    printf("Bit count          = %d bits/pixel\n", bmpInfoHeader->biBitCount);
+    printf("Compression        = %d\n", bmpInfoHeader->biCompression);
+    printf("Size image         = %d bytes\n", bmpInfoHeader->biSizeImage);
+    printf("X pixels per meter = %ld\n", bmpInfoHeader->biXPelsPerMeter);
+    printf("Y pixels per meter = %ld\n", bmpInfoHeader->biYPelsPerMeter);
+    printf("Color used         = %ld colors\n", bmpInfoHeader->biClrUsed);
+
+    int pixels = (int) (bmpInfoHeader->biWidth * bmpInfoHeader->biHeight);
+
+    // for(int i = 0; i < pixels; i++){
+    //     rgbtriple = rgbtri(fp);
+    //     // printf("%i %i %i\n", rgbtriple.blue, rgbtriple.green,  rgbtriple.red);
+
+    //     // hsvtriple = rgb2hsv(rgbtriple);
+    //     // printf("%lf %lf %lf\n", hsvtriple.hue, hsvtriple.saturation,  hsvtriple.value);
+
+    //     // if(hsvtriple.hue > 20 && hsvtriple.hue < 340){
+    //     //     hsvtriple.saturation = 0;
+    //     // }
+
+    //     // rgbtriple = hsv2rgb(hsvtriple);
+    //     fwrite(&rgbtriple.blue, 1, 1, fp2);
+    //     fwrite(&rgbtriple.green, 1, 1, fp2);
+    //     fwrite(&rgbtriple.red, 1, 1, fp2);
+    //     // printf("%i %i %i\n\n", rgbtriple.blue, rgbtriple.green,  rgbtriple.red);
+    // }
+
+    int i = 0;
+    fseek(fp, 0, SEEK_SET);
+    while(i < 1500){
+        fread(&buffer, 1, 1, fp);
+        printf("%i ",buffer);
+        i++;
+    }
+    puts("\n");
+    i = 0;
+    fseek(fp2, 0, SEEK_SET);
+    while(i < 1500){
+        fread(&buffer, 1, 1, fp2);
+        printf("%i ",buffer);
+        i++;
+    }	
+    free(bmpFileHeader);
+    free(bmpInfoHeader);
+
+    return 0;
+}
+
+BITMAPFILEHEADER *ReadBMFileHeader(FILE *fp, FILE *fp2){
+    BITMAPFILEHEADER *header;
+
+    WORD filetype;
+    DWORD filesize;
+    WORD reserved1;
+    WORD reserved2;
+    DWORD offset;
+
+    fread(&filetype, 1, 2, fp); 	//incorrect
+    fwrite(&filetype, 1, 2, fp2);
+
+    filesize = ReadLittleEndian4(fp,fp2);
+
+    fread(&reserved1, 1, 2, fp);
+    fwrite(&reserved1, 1, 2, fp2);
+
+    fread(&reserved2, 1, 2, fp);
+    fwrite(&reserved2, 1, 2, fp2);
+
+    offset = ReadLittleEndian4(fp,fp2);
+
+    header = (BITMAPFILEHEADER *) malloc(sizeof(BITMAPFILEHEADER));
+    header->bfType = filetype;
+    header->bfSize = filesize;
+    header->bfReserved1 = reserved1;
+    header->bfReserved2 = reserved2;
+    header->bfOffBits = offset;
+    
+    return header;
+}
+
+BITMAPINFOHEADER *ReadBMInfoHeader(FILE *fp, FILE *fp2){
+    BITMAPINFOHEADER *infoheader;
+
+    DWORD headersize;
+    LONG imagewidth;
+    LONG imageheight;
+    WORD noofcolorplanes;
+    WORD bitsperpixel;
+    DWORD compression;
+    DWORD imagesize;
+    LONG xresolutionpixelspermeter;
+    LONG yresolutionpixelspermeter;
+    DWORD usedcolors;
+    DWORD importantcolors;
+
+    headersize = ReadLittleEndian4(fp,fp2);
+    imagewidth = (LONG)ReadLittleEndian4(fp,fp2);
+    imageheight = (LONG)ReadLittleEndian4(fp,fp2);
+    noofcolorplanes = ReadLittleEndian2(fp,fp2);
+    bitsperpixel = ReadLittleEndian2(fp,fp2);
+    compression = ReadLittleEndian4(fp,fp2);
+    imagesize = ReadLittleEndian4(fp,fp2);
+    xresolutionpixelspermeter =(LONG) ReadLittleEndian4(fp,fp2);
+    yresolutionpixelspermeter = (LONG)ReadLittleEndian4(fp,fp2);
+    usedcolors = ReadLittleEndian4(fp,fp2);
+    importantcolors =  ReadLittleEndian4(fp,fp2);
+
+    infoheader = (BITMAPINFOHEADER *) malloc(sizeof(BITMAPINFOHEADER));
+    infoheader->biSize = headersize;
+    infoheader->biWidth = imagewidth;
+    infoheader->biHeight = imageheight;
+    infoheader->biPlanes = noofcolorplanes ;
+    infoheader->biBitCount = bitsperpixel;
+    infoheader->biCompression = compression;
+    infoheader->biSizeImage = imagesize;
+    infoheader->biXPelsPerMeter = xresolutionpixelspermeter;
+    infoheader->biYPelsPerMeter = yresolutionpixelspermeter;
+    infoheader->biClrUsed = usedcolors;
+    infoheader->biClrImportant = importantcolors;
+
+    return infoheader;
+}
+
+RGB rgbtri(FILE *fp){
+    RGB rgbtriple;
+
+    BYTE blue;
+    BYTE green;
+    BYTE red;
+
+    fread(&blue, 1, 1, fp);
+    fread(&green, 1, 1, fp); 
+    fread(&red, 1, 1, fp);
+
+    rgbtriple.blue = blue;
+    rgbtriple.green = green;
+    rgbtriple.red = red;
+
+    return rgbtriple;
+}
+
+DWORD ReadLittleEndian4(FILE *fp, FILE *fp2){
+    BYTE buf[4];
+    DWORD result = 0;  
+    int i;
+
+    fread(buf, 1 , 4, fp);
+    fwrite(buf, 1, 4, fp2);
+    for(i = 3; i >= 0; i--){
+        result = (result << 8) | (DWORD)buf[i];
+    }   
+    return result;
+}
+
+WORD ReadLittleEndian2(FILE *fp, FILE *fp2){
+    BYTE buf[2];
+    WORD result = 0;
+    int i = 0;
+
+    fread(buf, 1, 2, fp);
+    fwrite(buf, 1, 2, fp2);
+    for(i = 1; i >= 0; i--){
+        result = (result << 8) | (WORD)buf[i];
+    }
+    return result;
+}
